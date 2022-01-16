21bb27cd8de71fefdbbd5a63778d67dac63b2723
Karpff <57017382+Karpff@users.noreply.github.com>
2022-01-16 20:06:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,25 +15,63 @@ function getQuestion()
     }
     else 
     {
+        answersHTML = "";
         question = Math.floor(Math.random()*questions.length);
         $("#question")[0].innerHTML = questions[question].q;
-        rite = questions[question].a[0];
-        var points = [0,1,2,3];
-        for(let i=0;i<4;i++)
+        let answers = questions[question].a;
+        rite = answers[0];
+        var indexes = [];
+        for(let i in answers){indexes.push(i)}
+        for(let i=0;i<answers.length;i++)
         {
-            let nr = Math.floor(Math.random()*points.length)
-            $("#a"+i)[0].innerHTML = questions[question].a[points[nr]];
-            points.splice(nr,1);
+            let nr = Math.floor(Math.random()*indexes.length)
+            answersHTML += `<div class="alert alert-primary answer" id="a${i}">${answers[indexes[nr]]}</div>`
+            indexes.splice(nr,1);
         }
-        Array.from(document.getElementsByClassName("answer")).forEach(ele=>
-        {
-            ele.classList.remove("alert-danger");
-            ele.classList.remove("alert-success");
-            ele.classList.add("alert-primary");
-        });
+        $("#answers")[0].innerHTML = answersHTML;
         $("#questionSuccessRate")[0].innerHTML = ``
+        $(".answer").click(onAnswerClick);
     }
 }
+
+function onAnswerClick(e)
+{
+    if(!answered)
+    {
+        answered = true;
+        if(e.target.innerHTML == rite)
+        {
+            questions[question].right++;
+            rightAnswers++;
+            last20.push(true);
+            if(last20.length > 20)last20.shift();
+            $("#"+e.target.id).removeClass("alert-primary").addClass("alert-success");
+        }
+        else
+        {
+            questions[question].wrong++;
+            last20.push(false);
+            if(last20.length > 20)last20.shift();
+            $("#"+e.target.id).removeClass("alert-primary").addClass("alert-danger");
+            Array.from(document.getElementsByClassName("answer")).forEach(ele=>
+            {
+                if(ele.innerHTML == rite)
+                {
+                    ele.classList.remove("alert-primary");
+                    ele.classList.add("alert-success");
+                }
+            });
+        }
+        allAnswers++;
+        $("#progressLast20")[0].innerHTML = `${Math.floor(last20.filter(x=>x).length/last20.length*100)}%`;
+        $("#progressLast20").css("width",`${last20.filter(x=>x).length/last20.length*100}%`);
+        $("#progressAll")[0].innerHTML = `${Math.floor(rightAnswers/allAnswers*100)}%`;
+        $("#progressAll").css("width",`${rightAnswers/allAnswers*100}%`);
+        $("#questionSuccessRate")[0].innerHTML = `Twoje poprawne/niepoprawne odpowiedzi na to pytanie: <span class="badge badge-success">${questions[question].right}</span> | <span class="badge badge-danger">${questions[question].wrong}</span>`
+    }
+}
+
+
 window.addEventListener("load",_=>
 {
     getQuestion();
@@ -44,42 +82,7 @@ window.addEventListener("load",_=>
         q.wrong = 0;
     });
 
-    $(".answer").click(e=>
-    {
-        if(!answered)
-        {
-            answered = true;
-            if(e.target.innerHTML == rite)
-            {
-                questions[question].right++;
-                rightAnswers++;
-                last20.push(true);
-                if(last20.length > 20)last20.shift();
-                $("#"+e.target.id).removeClass("alert-primary").addClass("alert-success");
-            }
-            else
-            {
-                questions[question].wrong++;
-                last20.push(false);
-                if(last20.length > 20)last20.shift();
-                $("#"+e.target.id).removeClass("alert-primary").addClass("alert-danger");
-                Array.from(document.getElementsByClassName("answer")).forEach(ele=>
-                {
-                    if(ele.innerHTML == rite)
-                    {
-                        ele.classList.remove("alert-primary");
-                        ele.classList.add("alert-success");
-                    }
-                });
-            }
-            allAnswers++;
-            $("#progressLast20")[0].innerHTML = `${Math.floor(last20.filter(x=>x).length/last20.length*100)}%`;
-            $("#progressLast20").css("width",`${last20.filter(x=>x).length/last20.length*100}%`);
-            $("#progressAll")[0].innerHTML = `${Math.floor(rightAnswers/allAnswers*100)}%`;
-            $("#progressAll").css("width",`${rightAnswers/allAnswers*100}%`);
-            $("#questionSuccessRate")[0].innerHTML = `Twoje poprawne/niepoprawne odpowiedzi na to pytanie: <span class="badge badge-success">${questions[question].right}</span> | <span class="badge badge-danger">${questions[question].wrong}</span>`
-        }
-    });
+    $(".answer").click(onAnswerClick);
 
     $("#nextBtn").click(getQuestion);
     $("#deleteBtn").click(_=>
